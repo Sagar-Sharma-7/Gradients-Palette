8b57282bdd55fd37bc0cac5965ea4f77ee5b088b
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-07-25 13:19:11 +0530

    apply gradient using javascript by fetching json data'

--- public/js/jsonreader.js
+++ public/js/jsonreader.js
@@ -3,6 +3,7 @@ import data from './colors.json' assert { type: 'json' };
 console.log(data);
 
 const palettes_wrap = document.getElementsByClassName("palettes_wrap");
+// function to create palette divs
 const createPalettes = () => {
     let i = 1
     for(; i < data.length; i++){
@@ -11,5 +12,20 @@ const createPalettes = () => {
         palettes_wrap[0].appendChild(palette);
     }
 }
-
 createPalettes();
+
+// function to apply linear gradient to palette divs
+const palettes = document.querySelectorAll(".palette");
+const total_palettes = palettes.length;
+console.log(total_palettes)
+const applyGradient = () => {
+    let i = 0;
+    for(; i < total_palettes; i++){
+        palettes[i].style.background=`linear-gradient(to bottom right, ${data[i+1].colors[0]}, ${data[i+1].colors[1]})`;
+    }
+}
+
+applyGradient();
+
+
+
