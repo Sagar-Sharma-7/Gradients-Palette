4b1699a6728bab3a3314744c207ab5076e330652
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-07-25 16:00:55 +0530

    copy wrap hexcode and color box script

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,58 @@
+// function to fetch and read json file
+import data from './colors.json' assert { type: 'json' };
+
+const color = document.querySelectorAll(".color")
+const color_hex = document.querySelectorAll(".code")
+
+const palettes_wrap = document.getElementsByClassName("palettes_wrap");
+// function to create palette divs
+const createPalettes = () => {
+    let i = 1
+    for(; i < data.length; i++){
+        const palette = document.createElement("div");
+        palette.classList.add('palette');
+        palettes_wrap[0].appendChild(palette);
+    }
+}
+createPalettes();
+
+// function to apply linear gradient to palette divs
+const palettes = document.querySelectorAll(".palette");
+const total_palettes = palettes.length;
+// console.log(total_palettes)
+const applyGradient = () => {
+    let i = 0;
+    for(; i < total_palettes; i++){
+        palettes[i].style.background=`linear-gradient(to bottom right, ${data[i+1].colors[0]}, ${data[i+1].colors[1]})`;
+        palettes[i].setAttribute("data-color_one", data[i+1].colors[0]);
+        palettes[i].setAttribute("data-color_two", data[i+1].colors[1]);
+    };
+}
+
+applyGradient();
+
+// function to show copy wrap div when user click on palette
+palettes.forEach(item =>{
+    item.addEventListener("click", ()=> {
+        let color_one = item.getAttribute("data-color_one");
+        let color_two = item.getAttribute("data-color_two");
+        color_hex[0].innerHTML = color_one.toUpperCase();
+        color_hex[1].innerHTML = color_two.toUpperCase();
+        color[0].style.backgroundColor = color_one;
+        color[1].style.backgroundColor = color_two;
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
 // Function to change title when tab is switched
 const handleVisibilityChange = () => {
     if(document.hidden){
